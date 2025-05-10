41c4db5889ea66201c8744533b9710b8ed7c2120
Fernanda Brum <fernandab.geol@gmail.com>
2025-05-09 23:32:36 -0300

    Criacao repositorio

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,12 @@
+
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
