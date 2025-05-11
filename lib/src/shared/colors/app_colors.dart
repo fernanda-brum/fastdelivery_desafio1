7a1bba0e721b4762b4d8bdb6ae894739c4664eda
Fernanda Brum <fernandab.geol@gmail.com>
2025-05-10 21:06:54 -0300

    finalização do projeto

---
+++ lib/src/shared/colors/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const primary = Color(0xFFE53935);
+  static const danger  = Color(0xFFFFFFFF);
+}
---
+++ lib/src/shared/components/error_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ErrorWidget extends StatelessWidget {
+  final String message;
+
+  const ErrorWidget({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        message,
+        style: TextStyle(
+          color: Colors.red[50],
+          fontSize: 16,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
